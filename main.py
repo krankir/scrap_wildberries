acf4d64e5bf6ffb01906222e235b7a03fcd532c9
Анатолий Редько <stroitel.proekt@gmail.com>
2023-06-19 08:03:38 +0500

    finish him

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ import fake_useragent
 import requests
 import csv
 
+from models import Items
+
 
 class ScrapWB:
+    """Парсинг сайта."""
     def __init__(self, url: str):
         self.url = url
 
     def parse(self):
+        """Запрос и получение данных."""
         user = fake_useragent.UserAgent().random
 
         headers = {
@@ -22,14 +26,19 @@ class ScrapWB:
             'Referer': 'https://www.wildberries.ru/',
             'Connection': 'keep-alive',
         }
-        while True:
-            response = requests.get(
-                'https://catalog.wb.ru/brands/g/catalog?&appType=1&brand=28928&curr=rub&dest=-1579610&regions=80,38,4,64,83,33,68,70,69,30,86,40,1,66,110,22,31,48,114&sort=popular&spp=26',
-                headers=headers,
-            )
 
+        self.__create_csv()
+
+        response = requests.get(
+            'https://catalog.wb.ru/brands/g/catalog?&appType=1&brand=28928&curr=rub&dest=-1579610&regions=80,38,4,64,83,33,68,70,69,30,86,40,1,66,110,22,31,48,114&sort=popular&spp=26',
+            headers=headers,
+        )
+        print(response.json())
+        items_info = Items.parse_obj(response.json().get('data'))
+        self.__save_csv(items_info)
 
     def __create_csv(self):
+        """Создание файла с названием колонок."""
         with open('wb_data.csv', 'w', newline='') as file:
             writer = csv.writer(file)
             writer.writerow(['id',
@@ -41,8 +50,19 @@ class ScrapWB:
                              'в наличии']
                             )
 
-    def __save_csv(self):
-        ...
+    def __save_csv(self, items):
+        """Запись данных в заранее созданные колонки."""
+        with open('wb_data.csv', mode='a', newline='') as file:
+            writer = csv.writer(file)
+            for product in items.products:
+                writer.writerow([product.id,
+                                product.name,
+                                product.salePriceU,
+                                product.brand,
+                                product.sale,
+                                product.rating,
+                                product.volume]
+                                )
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -5,17 +5,19 @@ class Item(BaseModel):
     id: int
     name: str
     salePriceU: float
-    brand: int
+    brand: str
     sale: int
     rating: int
     volume: int
 
     @root_validator(pre=True)
-    def convert_sale_price(self, values: dict):
+    def convert_sale_price(cls, values: dict):
+        """Дополнительная обработка цены на товар."""
         sale_price = values.get('salePriceU')
         if sale_price is not None:
             values['salePriceU'] = sale_price / 100
         return values
 
+
 class Items(BaseModel):
-    products: list
+    products: list[Item]
